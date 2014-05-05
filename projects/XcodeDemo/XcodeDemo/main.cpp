0151264551abfea899cc8b96d4d5305c3cd7f4e9
anjsxz <anjsxz@gmail.com>
2014-05-05 23:38:59 +0800

    add

---
+++ projects/XcodeDemo/XcodeDemo/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  XcodeDemo
+//
+//  Created by anjun on 5/5/14.
+//  Copyright (c) 2014 anjun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
